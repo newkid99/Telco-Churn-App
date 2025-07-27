86982f2cfd7818d91fa6c8bee5e9d7d10cb55e1f
Kwadwo Jectey Nyarko <132113712+newkid99@users.noreply.github.com>
2025-07-27 22:49:44 +0000

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -30,7 +30,7 @@ st.image(logo,caption="",width=300)
 
 #Looading data at the Global level
 try:
-    dataset = pd.read_csv("C:/Users/kojon/PycharmProjects/SupervisedProjects/Supervised Projects/Customer-Churn.csv")
+    dataset = pd.read_csv(""data/Customer-Churn.csv")
 except FileNotFoundError as e :
     st.error(f"User, check if there is error loading dataset{e}")
     st.stop()
